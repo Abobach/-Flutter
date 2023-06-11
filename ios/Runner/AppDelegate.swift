246760330a75054ce61642189f7517c281e3db5a
Abobach <84929442+Abobach@users.noreply.github.com>
2023-06-12 00:07:43 +0300

    финальная версия

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import YandexMapsMobile
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-     YMKMapKit.setApiKey("067f1436-5a6a-43ec-a24d-06672d32a25e")
+     YMKMapKit.setApiKey("c2d1b3cc-e8d9-4e55-97b5-beefddd56c9b")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
